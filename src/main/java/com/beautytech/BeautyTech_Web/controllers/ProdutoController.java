1fa129fe889cf32614626a0d58d9b22c9d266b12
CarlosEduardo7700 <rm552164@fiap.com.br>
2024-09-29 16:02:00 -0300

    Criação da página para editar os Produtos em estoque

--- src/main/java/com/beautytech/BeautyTech_Web/controllers/ProdutoController.java
+++ src/main/java/com/beautytech/BeautyTech_Web/controllers/ProdutoController.java
@@ -7,8 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("produto")
@@ -16,6 +18,12 @@ public class ProdutoController {
 
     @Autowired
     private ProdutoRepository produtoRepository;
+
+    @GetMapping
+    public String exibirProdutos(Model model) {
+        model.addAttribute("produtos", produtoRepository.findAll());
+        return "produto/list-produtos";
+    }
 
     @GetMapping("cadastrar")
     public String exibirFormularioCadastrar(Model model) {
@@ -31,9 +39,18 @@ public class ProdutoController {
         return "produto/form-cadastrar";
     }
 
-    @GetMapping
-    public String exibirProdutos(Model model) {
-        model.addAttribute("produtos", produtoRepository.findAll());
-        return "produto/list-produtos";
+    @GetMapping("editar/{id}")
+    public String exibirFormularioEditar(@PathVariable("id") Long id, Model model) {
+        model.addAttribute("produto", produtoRepository.getReferenceById(id));
+        return "produto/form-editar";
     }
+
+    @PostMapping("editar")
+    public String editarProduto(Produto produto, RedirectAttributes redirectAttributes){
+        produtoRepository.save(produto);
+        redirectAttributes.addFlashAttribute("mensagem", "O produto foi atualizado!");
+        return "redirect:/produto";
+    }
+
+
 }
