5b6430e10437dcbf31e174d61a83dee125982bf1
CarlosEduardo7700 <rm552164@fiap.com.br>
2024-10-03 17:47:07 -0300

    Criação de widgets para a Home do projeto

--- src/main/java/com/beautytech/BeautyTech_Web/controllers/HomeController.java
+++ src/main/java/com/beautytech/BeautyTech_Web/controllers/HomeController.java
@@ -1,13 +1,40 @@
 package com.beautytech.BeautyTech_Web.controllers;
 
+import com.beautytech.BeautyTech_Web.models.Produto;
+import com.beautytech.BeautyTech_Web.repositories.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
 
 @Controller
 public class HomeController {
 
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
     @GetMapping
-    public String exibirHome() {
+    public String exibirHome(Model model) {
+
+        var quantidadeEmEstoque = 0;
+
+        for (Produto produto : produtoRepository.findAll()) {
+            quantidadeEmEstoque += produto.getQuantidadeEmEstoque();
+        }
+
+        model.addAttribute("quantidadeDeProdutos", quantidadeEmEstoque);
+
+        var produtosEmExcesso = produtoRepository.findAll().stream()
+                .filter(produto -> produto.getQuantidadeEmEstoque() > produto.getQuantidadeMaximaParaEstoque()).toList();
+
+        model.addAttribute("quantidadeDeProdutosEmExcesso", produtosEmExcesso.size());
+
+        var produtosEmFalta = produtoRepository.findAll().stream()
+                .filter(produto -> produto.getQuantidadeEmEstoque() < produto.getQuantidadeMinimaParaEstoque()).toList();
+
+        model.addAttribute("quantidadeDeProdutosEmFalta", produtosEmFalta.size());
+
         return "home";
     }
 }
