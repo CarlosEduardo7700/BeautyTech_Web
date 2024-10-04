85518d9b29043bc595a28afae55c8932438fb98d
CarlosEduardo7700 <rm552164@fiap.com.br>
2024-10-04 00:35:05 -0300

    Criação de uma barra de pesquisa para listar os produtos pelo nome

--- src/main/java/com/beautytech/BeautyTech_Web/controllers/ProdutoController.java
+++ src/main/java/com/beautytech/BeautyTech_Web/controllers/ProdutoController.java
@@ -6,10 +6,7 @@ import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
@@ -64,6 +61,12 @@ public class ProdutoController {
     public String exibirInformacoesDoProduto(@PathVariable("id") Long id, Model model) {
         model.addAttribute("produto", produtoRepository.getReferenceById(id));
         return "produto/detail-produto";
+    }
+
+    @GetMapping("pesquisar-por-nome")
+    public String exibirProdutosPesquisados(@RequestParam("nomePesquisa") String nomePesquisa, Model model) {
+        model.addAttribute("produtos", produtoRepository.findByNomeContainingIgnoreCase(nomePesquisa));
+        return "produto/list-produtos";
     }
 
 
--- src/main/java/com/beautytech/BeautyTech_Web/repositories/ProdutoRepository.java
+++ src/main/java/com/beautytech/BeautyTech_Web/repositories/ProdutoRepository.java
@@ -3,5 +3,8 @@ package com.beautytech.BeautyTech_Web.repositories;
 import com.beautytech.BeautyTech_Web.models.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+    List<Produto> findByNomeContainingIgnoreCase(String nome);
 }
