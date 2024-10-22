a24477fca8f0b4c7b4c23ca3c5cfc82f3d632aea
CarlosEduardo7700 <rm552164@fiap.com.br>
2024-10-21 21:38:34 -0300

    Implementação dos Repositories das Roles e Usuários.

---
+++ src/main/java/com/beautytech/BeautyTech_Web/repositories/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.beautytech.BeautyTech_Web.repositories;
+
+import com.beautytech.BeautyTech_Web.models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role,Long> {
+}
