c7621f0040d27910308499b97d4521c59161617b
CarlosEduardo7700 <rm552164@fiap.com.br>
2024-10-25 18:04:36 -0300

    Criação do Service de usuário e da tela de Login. Atualização do form de cadastro de usuário

--- src/main/java/com/beautytech/BeautyTech_Web/controllers/UsuarioController.java
+++ src/main/java/com/beautytech/BeautyTech_Web/controllers/UsuarioController.java
@@ -1,10 +1,14 @@
 package com.beautytech.BeautyTech_Web.controllers;
 
+import com.beautytech.BeautyTech_Web.dtos.usuario.CadastroDeUsuarioDto;
 import com.beautytech.BeautyTech_Web.repositories.RoleRepository;
+import com.beautytech.BeautyTech_Web.services.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -14,9 +18,19 @@ public class UsuarioController {
     @Autowired
     private RoleRepository roleRepository;
 
+    @Autowired
+    private UsuarioService usuarioService;
+
     @GetMapping("cadastrar")
-    public String exibirCadastroDeUsuario(Model model) {
+    public String exibirCadastroDeUsuario(CadastroDeUsuarioDto dto, Model model) {
+        model.addAttribute("usuario", dto);
         model.addAttribute("roles", roleRepository.findAll());
         return "usuario/form-cadastrar";
+    }
+
+    @PostMapping("cadastrar")
+    public String cadastrarUsuario(@ModelAttribute("usuario") CadastroDeUsuarioDto dto) {
+        usuarioService.saveUser(dto.email(), dto.senha(), dto.roles());
+        return "redirect:/login";
     }
 }
